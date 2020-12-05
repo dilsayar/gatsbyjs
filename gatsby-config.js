718a5e5f94d6b84fce202c3516b9a2660a364ffe
Hüseyin Demirtaş <1660961+dilsayar@users.noreply.github.com>
2020-12-06 01:36:16 +0300

    Update gatsby-config.js

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,11 +1,11 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Starter Blog`,
+    title: `Dilsayar Test Projesi`,
     author: {
-      name: `Kyle Mathews`,
-      summary: `who lives and works in San Francisco building useful things.`,
+      name: `Hüseyin Demirtaş`,
+      summary: `Dil ve teknoloji`,
     },
-    description: `A starter blog demonstrating what Gatsby can do.`,
+    description: `Gatsby test etmek için bir site.`,
     siteUrl: `https://gatsby-starter-blog-demo.netlify.app/`,
     social: {
       twitter: `kylemathews`,
